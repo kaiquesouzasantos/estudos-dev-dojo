ac2a06d744af4a00011e0926bfdd89ca155ac60f
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2023-01-18 08:47:28 -0300

    Delete SpringBootEssential2/test directory

--- SpringBootEssential2/test/java/com/example/springbootessential2/util/AnimePostRequestBodyCreator.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootessential2.util;
-
-import com.example.springbootessential2.request.AnimeRequestBody;
-
-public class AnimePostRequestBodyCreator {
-    public static AnimeRequestBody createAnimePostRequestBody(){
-        return new AnimeRequestBody(AnimeCreator.createAnimeToBeSaved().getNome());
-    }
-}
--- SpringBootEssential2/test/java/com/example/springbootessential2/util/AnimePutRequestBody.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootessential2.util;
-
-import com.example.springbootessential2.request.AnimeRequestPut;
-
-public class AnimePutRequestBody {
-    public static AnimeRequestPut createAnimePutRequestBody() {
-        return new AnimeRequestPut(AnimeCreator.createAnimeValid().getId(), AnimeCreator.createAnimeValid().getNome());
-    }
-}
